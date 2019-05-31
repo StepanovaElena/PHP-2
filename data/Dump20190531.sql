f696fb740b6c3f7abd75e8c7d9eae3df2d82c340
StepanovaElena <stepanova_eg@bk.ru>
2019-05-31 20:29:53 +0300

    Обновленная база

---
+++ data/Dump20190531.sql
@@ -0,0 +1,191 @@
+-- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: new_project
+-- ------------------------------------------------------
+-- Server version	8.0.15
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cart`
+--
+
+DROP TABLE IF EXISTS `cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `session_id` varchar(255) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `product_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `subtotal` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cart`
+--
+
+LOCK TABLES `cart` WRITE;
+/*!40000 ALTER TABLE `cart` DISABLE KEYS */;
+INSERT INTO `cart` VALUES (6,'9jd5f2n9kp96bdd03u5n266sdij5r0ei',NULL,2,4,'200'),(7,'9jd5f2n9kp96bdd03u5n266sdij5r0ei',NULL,3,2,'246');
+/*!40000 ALTER TABLE `cart` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `feedback`
+--
+
+DROP TABLE IF EXISTS `feedback`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `feedback` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `user_name` varchar(45) NOT NULL,
+  `text` varchar(500) NOT NULL,
+  `date` datetime DEFAULT NULL,
+  `likes` int(11) DEFAULT NULL,
+  `status` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `feedback`
+--
+
+LOCK TABLES `feedback` WRITE;
+/*!40000 ALTER TABLE `feedback` DISABLE KEYS */;
+INSERT INTO `feedback` VALUES (1,1,1,'User User','Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid architecto assumenda corporis debitis, delectus dignissimos explicabo illum, impedit ipsa itaque.','2019-05-23 00:00:00',2,'confirm'),(2,2,1,'User1 User1','Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid architecto assumenda corporis debitis, delectus dignissimos explicabo illum, impedit ipsa itaque','2019-05-23 00:00:00',3,'new'),(3,1,1,'Helen Hunt','Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid architecto assumenda corporis debitis, delectus dignissimos explicabo illum, impedit ipsa itaque.','2019-05-23 22:16:11',4,'new'),(4,1,1,'Helen Hunt','Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid architecto assumenda corporis debitis, delectus dignissimos explicabo illum, impedit ipsa itaque.','2019-05-23 22:16:58',5,'new'),(5,1,1,'Helen Hunt','Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid architecto assumenda corporis debitis, delectus dignissimos explicabo illum, impedit ipsa itaque.','2019-05-23 22:19:43',6,'new');
+/*!40000 ALTER TABLE `feedback` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `orders` (
+  `number` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `price` decimal(10,0) NOT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `photo` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'T-shirt','Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad at minus odio praesentium! Aperiam, autem deserunt dolores dolorum fuga labore odit quo.',11,NULL,'Layer_2.jpg'),(2,'Dress','Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad at minus odio praesentium! Aperiam, autem deserunt dolores dolorum fuga labore odit quo.',50,NULL,'Layer_2.jpg'),(3,'Suite','Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad at minus odio praesentium! Aperiam, autem deserunt dolores dolorum fuga labore odit quo.',123,NULL,'Layer_2.jpg'),(4,'T-sirt 2','Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad at minus odio praesentium! Aperiam, autem deserunt dolores dolorum fuga labore odit quo.',11,NULL,'Layer_2.jpg'),(5,'Dress 2','Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad at minus odio praesentium! Aperiam, autem deserunt dolores dolorum fuga labore odit quo.',50,NULL,'Layer_2.jpg'),(6,'Suite 2','Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad at minus odio praesentium! Aperiam, autem deserunt dolores dolorum fuga labore odit quo.',123,NULL,'Layer_2.jpg');
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(45) NOT NULL,
+  `nick_name` varchar(45) NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `is_confirmed` varchar(45) DEFAULT NULL,
+  `role` varchar(45) NOT NULL,
+  `hash` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'new_user1','User User1','$2y$10$SNhsB4JuEKsZhVDUn1pxvuGhrOV4nOA14Euokibv3beUcdaRrAK.m','new_user1@mail.ru','2019-05-31 14:05:35','','user','hash1'),(2,'new_user2','User User2','$2y$10$Acv4OXlmwx3/yh5T.eV.fe4hxmwJ2m1fBDdYCJdRRVRUzKXx1tHaG','new_user2@mail.ru','2019-05-31 14:06:26','','user','hash2'),(3,'admin','Admin','$2y$10$0mL7J6jF1teoa7u6p.x5Re/.lI83Il2bDJmnlGbDpo7bhszJ62Kpu','admin@mail.ru','2019-05-31 16:38:09','','user','4702799185cf143fb1937b2.93628431');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-05-31 20:28:28
